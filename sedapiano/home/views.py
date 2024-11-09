4603100f5e187689a47fbdbf6a942d5c94e634eb
Amirmbz <164421263+Amirmbz@users.noreply.github.com>
2024-11-09 16:59:53 +0330

    starting the project creating the homepage

---
+++ sedapiano/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
+from secrets import token_hex
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'home/index.html')
